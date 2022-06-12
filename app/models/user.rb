325cd1eebf7d4f5776ad26d89dec1a144ff89ead
elena-weber <crochetmiracles@gmail.com>
2022-06-12 22:24:02 +0000

    add slug to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,9 @@ class User < ApplicationRecord
   validate :must_have_a_role, on: :update
   
   has_many :courses
+  
+  extend FriendlyId
+  friendly_id :email, use: :slugged
          
   def to_s
     email
