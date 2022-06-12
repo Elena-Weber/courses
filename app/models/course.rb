417772efaec86027a5946ac08cad2d74ef40227e
elena-weber <crochetmiracles@gmail.com>
2022-06-12 23:27:01 +0000

    add lessons

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,7 @@ class Course < ApplicationRecord
     validates :title, :short_description, :language, :price, :level, presence: true
     validates :description, presence: true, length: { :minimum => 5 }
     belongs_to :user
+    has_many :lessons, dependent: :destroy
     
     extend FriendlyId
     friendly_id :title, use: :slugged
