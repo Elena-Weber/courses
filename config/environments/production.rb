6841b1e4b70e416ef41849dbbe1d1cfe5b2cd3a8
elena-weber <crochetmiracles@gmail.com>
2022-06-17 00:35:27 +0000

    change to smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,14 +12,11 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   
-  config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  # config.action_mailer.sendmail_settings = {
-  #   location: '/usr/sbin/sendmail',
-  #   arguments: '-i -t'
-  # }
+  config.action_mailer.default_url_options = { :host => 'coursella.herokuapp.com', :protocol => 'https' }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
 
   Rails.application.config.middleware.use ExceptionNotification::Rack,
   email: {
@@ -77,12 +74,6 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "courses_production"
-
-  config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
